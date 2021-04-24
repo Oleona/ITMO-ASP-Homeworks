dbba55df46e4ed042c9e32bd267d57b1f85f598e
Oleona <Ole-onna@mail.ru>
2021-04-24 22:58:57 +0300

    Lab4

---
+++ lab4/WingtipToys.Data/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace WingtipToys.Data
+{
+    public interface ICategoryRepository
+    {
+        Category Create(Category category);
+        Category Update(Category category);
+        bool Delete(int categoryId);
+        Category Get(int categoryId);
+        Category Get(string categoryName);
+        IEnumerable<Category> GetAll();
+    }
+}
---
+++ lab4/WingtipToys.Data/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace WingtipToys.Data
+{
+    public interface IProductRepository
+    {
+        Product Create(Product product);
+        Product Update(Product product);
+        bool Delete(int productId);
+        Product Get(int productId);
+        IEnumerable<Product> GetByCategoryId(int categoryId);
+        IEnumerable<Product> GetAll();
+    }
+}
