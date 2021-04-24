5fa18eb301c5b4473a03f007e8f2a4d19c101554
Oleona <Ole-onna@mail.ru>
2021-04-25 00:00:20 +0300

    Lab5 added

---
+++ lab5/MvcMusicStore/Models/MusicStoreDbInitializer.cs
@@ -0,0 +1,20 @@
+﻿namespace MvcMusicStore.Models
+{
+    public class MusicStoreDbInitializer
+        : System.Data.Entity.DropCreateDatabaseAlways<MvcMusicStoreDB>
+    {
+        protected override void Seed(MvcMusicStoreDB context)
+        {
+            context.Artists.Add(new Artist { Name = "Al Di Meola" });
+            context.Genres.Add(new Genre { Name = "Jazz" });
+            context.Albums.Add(new Album
+            {
+                Artist = new Artist { Name = "Rush" },
+                Genre = new Genre { Name = "Rock" },
+                Price = 9.99m,
+                Title = "Caravan"
+            });
+            base.Seed(context);
+        }
+    }
+}
