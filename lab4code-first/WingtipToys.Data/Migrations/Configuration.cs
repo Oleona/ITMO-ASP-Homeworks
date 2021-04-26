19f5f08728962e7ddb397b0af18c4a8c4e86560f
Oleona <Ole-onna@mail.ru>
2021-04-26 23:30:31 +0300

    Update for EntityFramework

---
+++ lab4code-first/WingtipToys.Data/Migrations/Configuration.cs
@@ -0,0 +1,17 @@
+﻿namespace WingtipToys.Data.Migrations
+{
+    using System.Data.Entity.Migrations;
+    using WingtipToys.Data.ModelsCodeFirst;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WingtipModelCodeFirst>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WingtipModelCodeFirst context)
+        {
+        }
+    }
+}
