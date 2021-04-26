03162fc5edf3e5c6619a75e7a53604d74f85dca0
Oleona <Ole-onna@mail.ru>
2021-04-26 16:49:25 +0300

    Lab7 in progress

---
+++ Lab7/MvcMusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,45 @@
+﻿using MvcMusicStore.Models;
+using MvcMusicStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+            // Set up our ViewModel
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal()
+            };
+
+
+             // Return the view
+            return View(viewModel);
+        }
+        
+        
+
+           
+        //
+        // GET: /ShoppingCart/CartSummary
+
+        [ChildActionOnly]
+        public ActionResult CartSummary()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            ViewData["CartCount"] = cart.GetCount();
+
+            return PartialView("CartSummary");
+        }
+    }
+}
